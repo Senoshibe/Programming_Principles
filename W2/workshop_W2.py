decf800291f807f25944f7aca7d577f4423f355f
Senoshibe <k_m_choi@outlook.com>
2024-08-11 21:06:59 +1000

    First commit

---
+++ W2/workshop_W2.py
@@ -0,0 +1,16 @@
+name = input("What's your name?")
+print("Hello, ", name)
+
+#Separator `sep=","` Print Function Example
+print("apple", "banana", "cherry")
+print("apple", "banana", "cherry", sep=", ")
+print("apple", "banana", "cherry", sep=" - ")   
+
+#`end` Print Function Example
+print("apple", end=", ")
+print("banana", end=", ")
+print("cherry", end=".\n")
+
+print("apple", end="-")
+print("banana", end="-")
+print("cherry", end=".")
