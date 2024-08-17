cf2de0530dde00c1595c2c7784692e4c5d202686
Senoshibe <k_m_choi@outlook.com>
2024-08-18 04:22:50 +1000

    Reference ID file
    Created a new file for reference IDs for variables

--- W2/workshop_escapesequences_W2.py
+++ W2/workshop_escapesequences_W2.py
@@ -1,3 +1,5 @@
 #the tab end sequence `\t` creates a tabbed space between objects
 
 print(5.2, 52, 3, sep="\t")
+
+#`\n` is a new line
--- W2/workshop_printingbackslash_W2.py
+++ W2/workshop_printingbackslash_W2.py
@@ -5,10 +5,10 @@ print("Hello", end="\")
 #this is because the backslash character `\` is a special character in python that is used to introduce escape sequences like `\n` for newline
 #therefore the correct way to print the backslash `\` is...
 
-print("Hello", end"\\")
+print("Hello", end="\\")
 
 #what if you want to print two backaslashes `\`? 
 
-print("Hello", end="\\\")
+print("Hello", end="\\\\")
       
     
