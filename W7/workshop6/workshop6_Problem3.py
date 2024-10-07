d3dfe5995221015932d0f9355fe809e7927df804
Senoshibe <k_m_choi@outlook.com>
2024-10-07 22:08:48 +1100

    Update workshop6_Problem3.py

--- W7/workshop6/workshop6_Problem3.py
+++ W7/workshop6/workshop6_Problem3.py
@@ -1,16 +1,10 @@
-import math
+import calendar
 
-room_width = float(input("Enter room dimension 1 (m): "))
-room_length = float(input("Enter room dimension 2 (m): "))
+start_year = int(input("Year 1? "))
+end_year = int(input("Year 2? "))
+               
+leap_days = int(calendar.leapdays(start_year, end_year + 1))
+              
+days = (end_year - start_year + 1) * 365 + leap_days
 
-print("Width of room = {:.3f}m".format(room_width) )
-print("Length of room = {:.3f}m".format(room_length))
-
-multiplier_lengthways = (room_width - (room_width % 3.66))/3.66
-total_carpet_lengthways = math.ceil(multiplier_lengthways * room_length)
-
-multiplier_widthways = (room_length - (room_length % 3.66))/3.66
-total_carpet_widthways = math.ceil(multiplier_widthways * room_width)
-
-print ("Total carpet length required in lengthways manner = ", int(total_carpet_lengthways), "m")
-print ("Total carpet length required in widthways manner = ", int(total_carpet_widthways), "m")
+print("Number of days: ", days)
